563763499eae138207a5ae97f5f35c9139cd2bec
V.Sadliy <sadliy@mail.ru>
2025-08-19 15:10:02 +0300

    Added solution extra level of task 2

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -29,4 +29,35 @@ function loadPoll() {
     };
 }
 
+// Отправлка голоса на сервер
+function sendVote(pollId, answerIndex) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/poll");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(`vote=${pollId}&answer=${answerIndex}`);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 201) {
+            const result = JSON.parse(xhr.responseText);
+            showResults(result.stat);
+        }
+    };
+}
+
+// Вычисление % и вывод статистики 
+function showResults(stat) {
+    pollAnswers.innerHTML = "";
+
+    const totalVotes = stat.reduce((sum, item) => sum + item.votes, 0);
+
+    stat.forEach(item => {
+        const percent = ((item.votes / totalVotes) * 100).toFixed(1);
+
+        const div = document.createElement("div");
+        div.textContent = `${item.answer}: ${percent}% (голосов - ${item.votes})`;
+
+        pollAnswers.appendChild(div);
+    });
+}
+
 loadPoll();
